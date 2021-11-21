27cc539b301d0c3a516950b5d584739e3e42d6cc
Andreas Botzler <andreas.botzler@myposter.de>
2021-11-21 20:10:08 +0100

    inital commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const semver  = require('semver');
+
+try {
+  const tag = core.getInput('latestTag');
+  const { runNumber, payload } = github.context;
+
+  const isPR = !!payload.pull_request
+  const prNumber = isPR ? payload.pull_request.number : null;
+  const branchRef = isPR ? payload.pull_request.head.ref : github.context.ref
+  const branchName = branchRef.replace('refs/heads/', '');
+  const currentVersion = semver.clean(tag)
+  if (!currentVersion) {
+    throw Error('Failed to extract latest release')
+  }
+
+  if (branchName === 'master') {
+    core.debug('Branch master');
+    core.setOutput('helmVersion', currentVersion);
+    return;
+  }
+
+  if (branchName === 'develop') {
+    core.debug('Branch develop');
+    const nextHelmVersion = semver.inc(currentVersion, 'prerelease', `dev-${runNumber}`)
+    core.setOutput('helmVersion', nextHelmVersion);
+    return;
+  }
+
+  if (isPR) {
+    core.debug('Pull Request');
+    const nextHelmVersion = semver.inc(currentVersion, 'prerelease', `PR-${prNumber}-${runNumber}`)
+    core.setOutput('helmVersion', nextHelmVersion);
+    return;
+  }
+
+  if (branchName.match('^(feature|bugfix)')) {
+    core.debug('feature/bugfix branch');
+    const preReleaseName = branchName.replace(/\//g, '-')
+    const nextHelmVersion = semver.inc(currentVersion, 'prerelease', `${preReleaseName}-${runNumber}`)
+    core.setOutput('helmVersion', nextHelmVersion);
+    return;
+  }
+
+
+  throw Error ('Branch is not allowed to get builded')
+} catch (error) {
+  core.setFailed(error.message);
+}
